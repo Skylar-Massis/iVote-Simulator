cfb64835d096672d876aa072065da23d49094b2e
Skylar-Massis <smassis@cpp.edu>
2026-06-12 03:48:19 -0700

    Update Question.java
    Added method to determine which questions allow multiple choices. Added method to create candidates.

--- Question.java
+++ Question.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
+//I did not include a way to ask the question or add the candidate options since it wouldn't be automated
+
+public class Question{
+    private boolean isMultiChoice;
+    private final List<String> candidates = new ArrayList<>();
+
+    void questionType(boolean multiChoice){
+        isMultiChoice = multiChoice;
+    }
+    boolean getIsMultiChoice(){
+        return isMultiChoice;
+    }
+    //Getter and setter functions for isMultiChoice
+
+    public void setCandidates(int numberOfAnswers) {
+        for (int i = 0; i < numberOfAnswers; i++) {
+            char letter = (char) ('A' + i);
+            candidates.add(String.valueOf(letter));
+        }
+    }
+    public List<String> getCandidates() {
+    return candidates;
+    }
+    //Setter and getter functions for Candidates
+
+    }
