d30c94aa7093e4ce22a04e15d304ac676a6fdbb0
Skylar-Massis <smassis@cpp.edu>
2026-06-12 03:52:15 -0700

    Update and rename StudentInterface.java to Voter.java
    Made a simple interface for classes that could be made in the future that might vote. This is used by the Student class.

---
+++ Voter.java
@@ -0,0 +1,10 @@
+interface Voter{
+
+public void setUniqueID(int newID);
+public int getUniqueID();
+//Setter and Getter for ID
+
+public void setVote(int i, boolean vote);
+public boolean[] getVoteList();
+//Setter and Getter for voteList
+}
