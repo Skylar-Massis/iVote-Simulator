95f66a07773a2936d241b6e545ac3674b46d0bd6
Skylar-Massis <smassis@cpp.edu>
2026-06-12 03:53:34 -0700

    Student.java now has functions and data
    Student class now with setter and getter functions for uniqueID and voteList

--- Student.java
+++ Student.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+class Student implements Voter {
 
+private int UniqueID;
+private boolean[] voteList;
+private static List<Integer> IDList = new ArrayList<>();
+
+@Override
+public void setUniqueID(int newID){
+        UniqueID = newID;
+        IDList.add(UniqueID);
+    }
+
+@Override
+    public int getUniqueID(){
+        return UniqueID;
+    }
+//Setter and Getter for UniqueID
+
+@Override
+public void setVote(int i, boolean vote){
+    voteList[i] = vote;
+    System.out.println(vote);
+    }
+
+@Override
+    public boolean[] getVoteList(){
+        return voteList;
+    }
+
+    public boolean getVote(int i){
+        return voteList[i];
+    }
+//Setter and Getter for voteList
+}
