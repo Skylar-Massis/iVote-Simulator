d2118c26a0ae3f38d008b4b85e513302fbb6483d
Skylar-Massis <smassis@cpp.edu>
2026-06-14 04:57:41 -0700

    Update Voter.java
    Added changeVote and changed getVoteList to an arraylist instead of a regular list

--- Voter.java
+++ Voter.java
@@ -1,10 +1,12 @@
+import java.util.List;
 interface Voter{
 
 public void setUniqueID(int newID);
 public int getUniqueID();
 //Setter and Getter for ID
 
-public void setVote(int i, boolean vote);
-public boolean[] getVoteList();
+public void setVote(boolean vote);
+public void changeVote(int index, boolean newVote);
+public List<Boolean> getVoteList();
 //Setter and Getter for voteList
 }
