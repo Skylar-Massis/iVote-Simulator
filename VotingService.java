09403fad7a898667209e202a6bb4d89ecfd9ad56
Skylar-Massis <smassis@cpp.edu>
2026-06-14 04:56:34 -0700

    Update VotingService.java
    There were many issues with this file but most of them are gone now.

--- VotingService.java
+++ VotingService.java
@@ -1,17 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 class VotingService{
-public static int[] storeVotes;
-    public void acceptVotes(Voter personalVotes){
-    storeVotes = new int[personalVotes.getVoteList().length];
-    for(int i = 0; i < personalVotes.getVoteList().length; i++){
-        if(personalVotes.getVoteList()[i] == true){
-        storeVotes[i] = storeVotes[i] + 1;
+    private List<Integer> storeVotes = new ArrayList<>();
+
+    public void acceptVotes(Voter personalVotes, Question q){
+    for(int i = 0; i < q.getCandidates().size(); i++){
+        if(personalVotes.getVoteList().get(i) && storeVotes.size() > i){
+        storeVotes.set(i, storeVotes.get(i) + 1);
     }
-    }
+       else if(storeVotes.size() < q.getCandidates().size()){
+        storeVotes.add(0);
+    }}
     }
 
-    public void displayStatistics(Question q){
-    System.out.print("Total Number of Students: " + q.getCandidates().size());
-    for (int i = 0; i < q.getCandidates().size(); i++){
-    System.out.println((char) ('A' + i) + ": " + storeVotes[i]);
+    public void displayStatistics(Question q, List<Student> studentList){
+    if(q.getAnswerType() == 0){
+    for (int i = 0; i < storeVotes.size(); i++){
+    System.out.println((char) ('A' +  i) + ": " + storeVotes.get(i) + " Votes");
+    }}else if(q.getAnswerType() == 1){
+    for (int i = 0; i < storeVotes.size(); i++){
+    System.out.println((i + 1) + "." + storeVotes.get(i) + " Votes");
+    }}else if(q.getAnswerType() == 2){
+    System.out.println("False: " + storeVotes.get(0) + " Votes");
+    System.out.println("True: " + storeVotes.get(1) + " Votes");
     }
-    }
+
+}
+}
+
+
