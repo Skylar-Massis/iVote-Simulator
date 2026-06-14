76682c43c85c0ca962d54aae29606895cfef2415
Skylar-Massis <smassis@cpp.edu>
2026-06-14 04:55:44 -0700

    Update Question.java
    Created answertype to change how the results are shown, edited the names to questionType instead of what it used to be.

--- Question.java
+++ Question.java
@@ -1,29 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//I did not include a way to ask the question or add the candidate options since it wouldn't be automated
-
 public class Question{
-    private boolean isMultiChoice;
+    private boolean questionType;
     private final List<String> candidates = new ArrayList<>();
-
-    void questionType(boolean multiChoice){
-        isMultiChoice = multiChoice;
-    }
-    boolean getIsMultiChoice(){
-        return isMultiChoice;
-    }
-    //Getter and setter functions for isMultiChoice
-
-    public void setCandidates(int numberOfAnswers) {
-        for (int i = 0; i < numberOfAnswers; i++) {
-            char letter = (char) ('A' + i);
-            candidates.add(String.valueOf(letter));
+    private int answerType;
+    void setQuestionType(boolean multiChoice){
+        questionType = multiChoice;
+        if(multiChoice){
+            System.out.println("Multiple Choice Question");
+        } else{
+            System.out.println("Single Choice Question");
         }
     }
+    boolean getQuestionType(){
+        return questionType;
+    }
+    //Setters and getters for QuestionType
+
+    void setAnswerType(int answerType){
+    this.answerType = answerType;
+    }
+    
+    int getAnswerType(){
+        return answerType;
+    }
+    //Setter and getters for answerType
+
+   public void setCandidates(int numberOfAnswers, int answerType) {
+    this.answerType = answerType;
+
+    if(answerType == 0){
+            for (int i = 0; i < numberOfAnswers; i++) {
+                char letter = (char) ('A' + i);
+                candidates.add(String.valueOf(letter));
+}}  if (answerType == 1){
+            for (int i = 0; i < numberOfAnswers; i++) {
+                candidates.add(String.valueOf(i + 1));
+}}  if (answerType == 2){
+            candidates.add("False");
+            candidates.add("True");
+            questionType = false;
+    }}
     public List<String> getCandidates() {
     return candidates;
     }
     //Setter and getter functions for Candidates
 
-    }
+}
