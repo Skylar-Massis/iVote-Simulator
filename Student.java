302ac215660f9e6713158db90477cff61dbedc0e
Skylar-Massis <smassis@cpp.edu>
2026-06-14 04:58:38 -0700

    Update Student.java
    Added changeVote and changed voteList to an arraylist

--- Student.java
+++ Student.java
@@ -3,34 +3,34 @@ import java.util.List;
 class Student implements Voter {
 
 private int UniqueID;
-private boolean[] voteList;
+private List<Boolean> voteList = new ArrayList<>();
+//private boolean[] voteList;
 private static List<Integer> IDList = new ArrayList<>();
 
 @Override
 public void setUniqueID(int newID){
-        UniqueID = newID;
-        IDList.add(UniqueID);
-    }
+    UniqueID = newID;
+    IDList.add(UniqueID);
+}
 
 @Override
-    public int getUniqueID(){
-        return UniqueID;
-    }
+public int getUniqueID(){
+    return UniqueID;
+}
 //Setter and Getter for UniqueID
 
 @Override
-public void setVote(int i, boolean vote){
-    voteList[i] = vote;
-    System.out.println(vote);
-    }
+public void setVote(boolean vote){
+    voteList.add(vote);
+}
 
 @Override
-    public boolean[] getVoteList(){
-        return voteList;
-    }
-
-    public boolean getVote(int i){
-        return voteList[i];
-    }
+public List<Boolean> getVoteList(){
+    return voteList;
+}
+@Override
+public void changeVote(int index, boolean newVote){
+    voteList.set(index, newVote);
+}
 //Setter and Getter for voteList
 }
