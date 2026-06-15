4c596b4815fa216efc7d1f3fc6ff4fc8a320db01
Skylar-Massis <smassis@cpp.edu>
2026-06-15 04:59:21 -0700

    Update VotingService.java
    Fixed mistakes and added comments

--- VotingService.java
+++ VotingService.java
@@ -1,33 +1,37 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 class VotingService{
     private List<Integer> storeVotes = new ArrayList<>();
 
-    public void acceptVotes(Voter personalVotes, Question q){
-    for(int i = 0; i < q.getCandidates().size(); i++){
-        if(personalVotes.getVoteList().get(i) && storeVotes.size() > i){
-        storeVotes.set(i, storeVotes.get(i) + 1);
-    }
-       else if(storeVotes.size() < q.getCandidates().size()){
-        storeVotes.add(0);
-    }}
-    }
-
-    public void displayStatistics(Question q, List<Student> studentList){
-    if(q.getAnswerType() == 0){
-    for (int i = 0; i < storeVotes.size(); i++){
-    System.out.println((char) ('A' +  i) + ": " + storeVotes.get(i) + " Votes");
-    }}else if(q.getAnswerType() == 1){
-    for (int i = 0; i < storeVotes.size(); i++){
-    System.out.println((i + 1) + "." + storeVotes.get(i) + " Votes");
-    }}else if(q.getAnswerType() == 2){
-    System.out.println("False: " + storeVotes.get(0) + " Votes");
-    System.out.println("True: " + storeVotes.get(1) + " Votes");
+    // This function traverses the list of candidates.
+    // First if makes space in the storeVotes Arraylist.
+    // Second if stores votes into storeVotes if there is a positive vote.
+    // I used voter instead of student because it should be able to work with any voter.
+    public void acceptVotes(Voter studentVotes, Question q){
+        for(int i = 0; i < q.getCandidates().size(); i++){
+            if(storeVotes.size() < q.getCandidates().size()){
+                storeVotes.add(0);
+            }if(studentVotes.getVoteList().get(i)){
+                storeVotes.set(i, storeVotes.get(i) + 1);
+            }
+        }
     }
 
+    // This function will display the statistic based on the way the answers are formatted.
+    // I chose to do this because it's pretty simple to modify and I think it's easy to add on to.
+    public void displayStatistics(Question q, List studentList){
+        if(q.getAnswerType() == 0){
+            for (int i = 0; i < storeVotes.size(); i++){
+                System.out.println((char) ('A' +  i) + ":  " + storeVotes.get(i) + " Votes");
+            }
+        }else if(q.getAnswerType() == 1){
+            for (int i = 0; i < storeVotes.size(); i++){
+                System.out.println((i + 1) + ":  " + storeVotes.get(i) + " Votes");
+            }
+        }else if(q.getAnswerType() == 2){
+            System.out.println("False:  " + storeVotes.get(0) + " Votes");
+            System.out.println("True:   " + storeVotes.get(1) + " Votes");
+        }
+    }
 }
-}
-
-
