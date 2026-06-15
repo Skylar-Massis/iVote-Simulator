daee225fa54537f5d0b5a65747047007410a5923
Skylar-Massis <smassis@cpp.edu>
2026-06-15 04:57:59 -0700

    Changed some names and added comments

--- Voter.java
+++ Voter.java
@@ -1,12 +1,14 @@
 import java.util.List;
+
+// I made this interface because it suited the way I was designing the students class.
+// Methods that use student as a parameter can use voter as well now.
 interface Voter{
+    public void setUniqueID(int newID);
+    public int getUniqueID();
+    //Setter and Getter for ID
 
-public void setUniqueID(int newID);
-public int getUniqueID();
-//Setter and Getter for ID
-
-public void setVote(boolean vote);
-public void changeVote(int index, boolean newVote);
-public List<Boolean> getVoteList();
-//Setter and Getter for voteList
+    public void addVote(boolean vote);
+    public void setVote(int index, boolean newVote);
+    public List<Boolean> getVoteList();
+    //Setter and Getter and function to add to voteList
 }
