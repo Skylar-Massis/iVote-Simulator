10f95ec1d7236de291cc3371505e72dfb1cc97ca
Skylar-Massis <smassis@cpp.edu>
2026-06-15 04:57:35 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,35 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 class Student implements Voter {
 
-private int UniqueID;
-private List<Boolean> voteList = new ArrayList<>();
-//private boolean[] voteList;
-private static List<Integer> IDList = new ArrayList<>();
+    private int UniqueID;
+    private List<Boolean> voteList = new ArrayList<>();
+    private static List<Integer> IDList = new ArrayList<>();
 
 @Override
-public void setUniqueID(int newID){
-    UniqueID = newID;
-    IDList.add(UniqueID);
-}
+    public void setUniqueID(int newID){
+        UniqueID = newID;
+        IDList.add(UniqueID);
+    }
 
 @Override
-public int getUniqueID(){
-    return UniqueID;
-}
-//Setter and Getter for UniqueID
+    public int getUniqueID(){
+        return UniqueID;
+    }
+    //Setter and Getter for UniqueID
 
 @Override
-public void setVote(boolean vote){
-    voteList.add(vote);
-}
+    public void addVote(boolean vote){
+        voteList.add(vote);
+    }
 
 @Override
-public List<Boolean> getVoteList(){
-    return voteList;
-}
+    public List<Boolean> getVoteList(){
+        return voteList;
+    }
+
 @Override
-public void changeVote(int index, boolean newVote){
-    voteList.set(index, newVote);
-}
-//Setter and Getter for voteList
+    public void setVote(int index, boolean newVote){
+        voteList.set(index, newVote);
+    }
+
+//Setter and Getter for voteList and a function that adds a vote to the voteList array.
 }
