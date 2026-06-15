b283b67278fd0278a374496a7c831ef5baa6de21
Skylar-Massis <smassis@cpp.edu>
2026-06-15 04:55:32 -0700

    Update SimluationDriver.java
    Finished and got fixed incorrectly counting votes

--- SimluationDriver.java
+++ SimluationDriver.java
@@ -2,64 +2,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 public class SimluationDriver{
+    
+    // The student IDs are stored but not used in this program, it is only used for the .size() method.
+    private List<Student> studentList = new ArrayList<>();
 
-public void main(String[] args) {
-    Question q = new Question();
-    VotingService v = new VotingService();
-    simulator(q, v);
-}
+    public void main(String[] args) {
+        simulationDriver();
+    }
 
-//can use hashset to keep it unique
-List<Student> studentList = new ArrayList<>();
+    public void simulationDriver(){
+        Question q = new Question();
+        VotingService v = new VotingService();
+        generateQuestion(q);
+        generateStudents(q, v);
+        System.out.println("Total Number of Students: " + studentList.size());
+        v.displayStatistics(q, studentList);
+    }
 
-
-
-
-public void simulator(Question q, VotingService v){
-    generateQuestion(q);
-    generateStudents(q, v);
-    System.out.println("Total Number of Students: " + studentList.size());
-    v.displayStatistics(q, studentList);
-}
-// There is an issue where there is 1 student who isn't voting.
-public void generateStudents(Question q, VotingService v){
-    for (int i = 0; i < randomNumber(10, 30); i++){
-        Student s = new Student();
-        s.setUniqueID(randomNumber(1000, 10000));
-        studentList.add(s);
-        if(q.getQuestionType() == true){
-            for (int g = 0; g < q.getCandidates().size(); g++){     //multiple choice questions
-            s.setVote(randomBoolean());
+    // This method will generate a random number of students
+    // With those students it will set the IDs and add them to the studentList
+    // it will then generate votes for those students and put them the votingService object.
+    public void generateStudents(Question q, VotingService v){
+        for (int i = 0; i < randomNumber(10, 20); i++){
+            Student s = new Student();
+            s.setUniqueID(randomNumber(100000, 1000000));
+            studentList.add(s);
+            if(q.getQuestionType() == true && q.getAnswerType() != 2){
+                for (int g = 0; g < q.getCandidates().size(); g++){
+                s.addVote(randomBoolean());
+                }
+            }
+            else{
+                for (int g = 0; g < q.getCandidates().size(); g++){  
+                s.addVote(false);
+                }
+                s.setVote(randomNumber(0, q.getCandidates().size() - 1), true);
+            }
+            v.acceptVotes(s, q);
         }
-        prepareVotes(s, v, q);
-        }else{
-            for (int g = 0; g < q.getCandidates().size(); g++){  
-            s.setVote(false);
-            }s.changeVote(randomNumber(0, q.getCandidates().size() - 1), true);
-             prepareVotes(s, v, q);
-        }
-}
-}
+    }
 
-public void generateQuestion(Question q){
-    q.setCandidates(randomNumber(2, 6), randomNumber(0,2));
-    q.setQuestionType(randomBoolean());
-}
+    // This method will generate a question with 2 to 6 possible answers
+    public void generateQuestion(Question q){
+        q.setCandidates(randomNumber(2, 6), randomNumber(0,2));
+        q.setQuestionType(randomBoolean());
+    }
 
-public void prepareVotes(Voter s, VotingService v, Question q){
-    v.acceptVotes(s, q);
-}
+    // These random methods are necessary for simulationDriver to give random results.
+    public boolean randomBoolean(){
+        Random random = new Random();
+        boolean randomBool = random.nextBoolean();
+        return randomBool;
+    }
 
-public boolean randomBoolean(){
-    Random random = new Random();
-    boolean randomBool = random.nextBoolean();
-    return randomBool;
-}
-
-public int randomNumber(int start, int end){
-    Random random = new Random();
-    int number = random.nextInt((end - start) + 1 ) + start;
-    return number;
-}
-
+    public int randomNumber(int start, int end){
+        Random random = new Random();
+        int number = random.nextInt((end - start) + 1 ) + start;
+        return number;
+    }
 }
